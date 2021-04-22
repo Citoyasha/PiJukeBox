130361198eae0cbfd1da74b224a57ac7f8326acc
Citoyasha <citoyasha@mail.ru>
2021-04-22 18:56:20 +0100

    added admin page & changed client page

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+
+const {exec, spawn} = require("child_process");
+
+const fs = require("fs");
+
+const client = express();
+
+const usetube = require("usetube");
+
+client.use(express.static('client')); //runs all file under ./client
+
+client.use(function (request, response) {
+  response.statusCode = 404;
+  response.redirect('main.html');
+});
+
+const io_client = require("http").createServer(client).listen(8080);
+
+const io = require("socket.io")(io_client);
+
+var yt_arr = [];
+var playing = 0;
+
+function play() {
+  if (yt_arr.length > 0) {
+    console.log(yt_arr);
+    const mpv = spawn("cvlc", [ "--play-and-exit", yt_arr[0] ]);
+    mpv.on("close", () => {
+      console.log("Playing Next!");
+      playing = 0;
+      yt_arr.shift();
+      play();
+    });
+  }
+}
+io.on("connection", (socket) => {
+  socket.on("message", (data) => {
+  console.log(data);
+  let cmd = data.substr(0, 2);
+  let arg = data.slice(2);
+switch(cmd){
+case "!s":
+  exec("killall -9 vlc");
+  playing = 0;
+  break;
+case "!v":
+  exec(`amixer set 'Master' ${arg}%`);
+  break;
+case "!m":
+  usetube.searchVideo(arg).then((r) => {
+    socket.emit("search0", r.videos[0].original_title, `https://i.ytimg.com/vi/${r.videos[0].id}/hqdefault.jpg`,
+                `https://www.youtube.com/watch?v=${r.videos[0].id}`);
+    socket.emit("search1", r.videos[1].original_title, `https://i.ytimg.com/vi/${r.videos[1].id}/hqdefault.jpg`,
+                `https://www.youtube.com/watch?v=${r.videos[1].id}`);
+    socket.emit("search2", r.videos[2].original_title, `https://i.ytimg.com/vi/${r.videos[2].id}/hqdefault.jpg`,
+                `https://www.youtube.com/watch?v=${r.videos[2].id}`);
+  });
+  break;
+case "!l":
+  yt_arr.push(arg);
+  if (playing == "0") {
+    play();
+    playing = 1;
+  }
+  break;
+default:
+  console.log("unknow value");
+    }
+  });
+});
+
